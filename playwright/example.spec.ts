d4f7be3659fd33b4f79014807c2349db12058429
szymon podstawski <szymon.podstawski@gmail.com>
2024-04-13 11:50:58 +0200

    nowy 12

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-
+//
 test('test', async ({ page }) => {
   await page.goto('https://www.onet.pl/');
   await page.getByLabel('accept and close').click();
