c8d7b882106878274d244da72f9f97f22c9e701b
szymon podstawski <szymon.podstawski@gmail.com>
2024-04-13 11:19:53 +0200

    nowe

---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.onet.pl/');
+  await page.getByLabel('accept and close').click();
+  await page.getByRole('navigation').getByRole('link', { name: 'Wiadomości' }).click();
+});
